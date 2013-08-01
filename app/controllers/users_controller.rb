d5e9f83bc5cb7aaeff70318a0cdf4a8ba0e0b6d3
Andy Brown <asbrown002@gmail.com>
2013-08-01 10:29:52 -0500

    Rank sorts ascending, not descending.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
 	def index
 		params[:page] ||=1
-		@users = User.order('rank desc').paginate(:page => params[:page], :per_page => 20)
+		@users = User.order('rank asc').paginate(:page => params[:page], :per_page => 20)
 		#@users = User.all.sort_by(&:rank)
 	end
 
