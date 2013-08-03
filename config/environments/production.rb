154eb798e64a2cc9c2237d6222438dc169adecf6
Andy Brown <asbrown002@gmail.com>
2013-08-03 14:41:15 -0500

    Production config for js-routes.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Pingpong::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.initialize_on_precompile = false
+  config.assets.initialize_on_precompile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
