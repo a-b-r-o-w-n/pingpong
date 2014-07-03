8d63597b317784afd370de1d8d8f23c145d58baa
Andy Brown <asbrown002@gmail.com>
2014-07-03 15:09:32 -0500

    Add host to production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,5 @@ Pingpong::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.default_url_options = { :host => 'fapingpong.herokuapp.com' }
 end
