0a4feed9ee5bb16efc203c0b4ffaee79a05bc099
Andy Brown <asbrown002@gmail.com>
2014-07-03 14:14:08 -0500

    Only seed in development

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,118 +8,119 @@
 
 DEFAULT_INSECURE_PASSWORD = 'password'
 
-andy = User.create({
-    first_name: "Andy",
-    last_name: "Brown",
-    profile_name: "andy",
-    email: "asbrown002@gmail.com",
-    password: DEFAULT_INSECURE_PASSWORD,
-    password_confirmation: DEFAULT_INSECURE_PASSWORD
-    })  
+if Rails.env.development?
+    andy = User.create({
+        first_name: "Andy",
+        last_name: "Brown",
+        profile_name: "andy",
+        email: "asbrown002@gmail.com",
+        password: DEFAULT_INSECURE_PASSWORD,
+        password_confirmation: DEFAULT_INSECURE_PASSWORD
+        })
 
-daniel = User.create({
-    first_name: "Daniel",
-    last_name: "Crawford",
-    profile_name: "daniel",
-    email: "daneil@example.com",
-    password: DEFAULT_INSECURE_PASSWORD,
-    password_confirmation: DEFAULT_INSECURE_PASSWORD
-    })
+    daniel = User.create({
+        first_name: "Daniel",
+        last_name: "Crawford",
+        profile_name: "daniel",
+        email: "daneil@example.com",
+        password: DEFAULT_INSECURE_PASSWORD,
+        password_confirmation: DEFAULT_INSECURE_PASSWORD
+        })
 
-michael = User.create({
-    first_name: "Michael",
-    last_name: "Gaddy",
-    profile_name: "michael",
-    email: "michael@example.com",
-    password: DEFAULT_INSECURE_PASSWORD,
-    password_confirmation: DEFAULT_INSECURE_PASSWORD
-    })
+    michael = User.create({
+        first_name: "Michael",
+        last_name: "Gaddy",
+        profile_name: "michael",
+        email: "michael@example.com",
+        password: DEFAULT_INSECURE_PASSWORD,
+        password_confirmation: DEFAULT_INSECURE_PASSWORD
+        })
 
-kellen = User.create({
-    first_name: "Kellen",
-    last_name: "Summer",
-    profile_name: "kellen",
-    email: "kellen@example.com",
-    password: DEFAULT_INSECURE_PASSWORD,
-    password_confirmation: DEFAULT_INSECURE_PASSWORD
-    })
+    kellen = User.create({
+        first_name: "Kellen",
+        last_name: "Summer",
+        profile_name: "kellen",
+        email: "kellen@example.com",
+        password: DEFAULT_INSECURE_PASSWORD,
+        password_confirmation: DEFAULT_INSECURE_PASSWORD
+        })
 
-Tournament.create({
-    tournament_type: 'SE',
-    num_players: 3
-    })
+    Tournament.create({
+        tournament_type: 'SE',
+        num_players: 3
+        })
 
-Tournament.create({
-    tournament_type: 'WS',
-    num_players: 3
-    })
+    Tournament.create({
+        tournament_type: 'WS',
+        num_players: 3
+        })
 
 
-Match.create({
-    status:         'P',
-    player1_id:     andy.id, 
-    score1:         11,
-    player2_id:     michael.id,
-    score2:         8,
-    tournament_id:  1
-	})
+    Match.create({
+        status:         'P',
+        player1_id:     andy.id,
+        score1:         11,
+        player2_id:     michael.id,
+        score2:         8,
+        tournament_id:  1
+    	})
 
-Match.create({
-    status:         'IP',
-    player1_id:     daniel.id, 
-    score1:         11,
-    player2_id:     andy.id,
-    score2:         6,
-    tournament_id:  1
-	})
+    Match.create({
+        status:         'IP',
+        player1_id:     daniel.id,
+        score1:         11,
+        player2_id:     andy.id,
+        score2:         6,
+        tournament_id:  1
+    	})
 
-Match.create({
-    status:         'C',
-    player1_id:     kellen.id,
-    score1:         11,
-    player2_id:     michael.id,
-    score2:         8,
-    tournament_id:  1
-    })
+    Match.create({
+        status:         'C',
+        player1_id:     kellen.id,
+        score1:         11,
+        player2_id:     michael.id,
+        score2:         8,
+        tournament_id:  1
+        })
 
-Match.create({
-    status:         'C',
-    player1_id:     kellen.id,
-    score1:         11,
-    player2_id:     andy.id,
-    score2:         6,
-    tournament_id:  2
-    })
+    Match.create({
+        status:         'C',
+        player1_id:     kellen.id,
+        score1:         11,
+        player2_id:     andy.id,
+        score2:         6,
+        tournament_id:  2
+        })
 
-Match.create({
-    status:         'C',
-    player1_id:     kellen.id,
-    score1:         11,
-    player2_id:     daniel.id,
-    score2:         6,
-    tournament_id:  2
-    })
+    Match.create({
+        status:         'C',
+        player1_id:     kellen.id,
+        score1:         11,
+        player2_id:     daniel.id,
+        score2:         6,
+        tournament_id:  2
+        })
 
-Match.create({
-    status:         'C',
-    player1_id:     michael.id,
-    score1:         11,
-    player2_id:     daniel.id,
-    score2:         6,
-    tournament_id:  2
-    })
+    Match.create({
+        status:         'C',
+        player1_id:     michael.id,
+        score1:         11,
+        player2_id:     daniel.id,
+        score2:         6,
+        tournament_id:  2
+        })
 
-Match.create({
-    status:         'C',
-    player1_id:     andy.id,
-    score1:         11,
-    player2_id:     kellen.id,
-    score2:         6,
-    tournament_id:  2
-    })
+    Match.create({
+        status:         'C',
+        player1_id:     andy.id,
+        score1:         11,
+        player2_id:     kellen.id,
+        score2:         6,
+        tournament_id:  2
+        })
 
-Match.all.each do |m|
-    TournamentUser.create(user_id: m.player1_id, tournament_id: m.tournament_id)
-    TournamentUser.create(user_id: m.player2_id, tournament_id: m.tournament_id)
+    Match.all.each do |m|
+        TournamentUser.create(user_id: m.player1_id, tournament_id: m.tournament_id)
+        TournamentUser.create(user_id: m.player2_id, tournament_id: m.tournament_id)
+    end
 end
-
